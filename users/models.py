b81c2b869dd73b252227cd9280a6696ead023f47
DONGHOON IM <dongttang@gmail.com>
2020-11-08 22:08:26 +0900

    Complete modeling user #5

--- users/models.py
+++ users/models.py
@@ -1,9 +1,41 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 
 
 class User(AbstractUser):
+    class Gender(models.TextChoices):
+        MALE = "M", _("male")
+        FEMALE = "F", _("female")
+        OTHER = "O", _("other")
 
-    test_field = models.TextField(default="")
+    class Language(models.TextChoices):
+        LANGUAGE_ENGLISH = "EN", _("English")
+        LANGUAGE_KOREAN = "KO", _("한국어")
+        LANGUAGE_JAPANESE = "JP", _("日本語")
+
+    class Currency(models.TextChoices):
+        USD = "USD", _("USD")
+        KRW = "KRW", _("KRW")
+        JPY = "JPY", _("JPY")
+
+    avata = models.ImageField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices, null=True)
+    bio = models.TextField(default="")
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        max_length=2,
+        choices=Language.choices,
+        null=False,
+        default=Language.LANGUAGE_ENGLISH,
+    )
+    currency = models.CharField(
+        max_length=3,
+        choices=Currency.choices,
+        null=False,
+        default=Currency.USD,
+    )
+    superhost = models.BooleanField(default=False, blank=False)
