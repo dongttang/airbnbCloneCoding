dd756880301e7f8e036607797bc4b5fe43d52b65
DONGHOON IM <dongttang@gmail.com>
2020-11-08 22:07:20 +0900

    Complete admin.py in users app #5

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            (
+                "Custom profile",
+                {
+                    "fields": (
+                        "avata",
+                        "gender",
+                        "bio",
+                        "birthday",
+                        "language",
+                        "currency",
+                        "superhost",
+                    )
+                },
+            )
+        ),
+    )
