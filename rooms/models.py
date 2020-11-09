8aae313b1f435e7e961d4127a68baff413e3a276
DONGHOON IM <dongttang@gmail.com>
2020-11-10 00:10:52 +0900

    Define room model #6

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
 # Create your models here.
+
+
+class Room(core_models.AbstractTimeStampModel):
+
+    host = models.ForeignKey(user_models.User)
+    room_title = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField()
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
