b1a88fef77a3a4c850e744c6bf15ab416662f11c
DONGHOON IM <dongttang@gmail.com>
2020-11-14 17:46:41 +0900

    Add comment

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,14 +11,23 @@ from . import models as rooms_models
     rooms_models.HouseRule,
 )
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(rooms_models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+    
     pass
 
 
 @admin.register(rooms_models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
+
     pass
