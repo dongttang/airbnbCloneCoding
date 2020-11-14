1e0f819597da2e2b16aa53d8f95c3a850353ff43
DONGHOON IM <dongttang@gmail.com>
2020-11-14 19:17:27 +0900

    Add reservation model #10

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core import models as core_models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Reservation(core_models.AbstractTimeStampModel):
+
+    """ Reservation Model Definition """
+
+    class ReservationStatus(models.TextChoices):
+        confirmed = "CONFIRMED", _("Confirmed")
+        pending = "PENDING", _("Pending")
+        canceled = "CANCELED", _("Canceled")
+
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE, null=False
+    )
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE, null=False
+    )
+    check_in_time = models.DateField()
+    check_out_time = models.DateField()
+    reservation_status = models.CharField(
+        max_length=10,
+        choices=ReservationStatus.choices,
+        null=False,
+        default=ReservationStatus.pending,
+    )
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in_time}"
