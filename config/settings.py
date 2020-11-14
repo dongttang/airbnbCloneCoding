dced99cb9c21c315b249e8edd10df6cd46c064e2
DONGHOON IM <dongttang@gmail.com>
2020-11-14 18:56:23 +0900

    Include PROJECT_APPS reservation #10

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ PRROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationConfig",
 ]
 
 THIRD_PARTY_APPS = ["django_countries"]
